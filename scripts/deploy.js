28c7689abe78036e5353a36c267888944908a9ff
maxwell-morgan <maxwell@corduroycloud.com>
2023-09-26 11:11:02 -0600

    Adds smart contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,27 @@
-async function main() {
-	// Fetch contract to deploy
-	const Token = await ethers.getContractFactory("Token_BBD")
+const hre = require("hardhat");
 
-	// Deploy contract
-	const token = await Token.deploy('BigBollaDolla', 'BBD', '1000000')
-	await token.deployed()
-	console.log(`Token Deployed to: ${token.address}`)
+async function main() {
+  const NAME = 'BigBollaDolla'
+  const SYMBOL = 'BBD'
+  const MAX_SUPPLY = '1000000'
+  const PRICE = ethers.utils.parseUnits('0.025', 'ether')
+
+  const Token = await hre.ethers.getContractFactory('Token_BBD')
+  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+
+  await token.deployed()
+  console.log(`Token deployed to:${token.address}\n`)
+
+  const Crowdsale = await hre.ethers.getContractFactory('Crowdsale')
+  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await crowdsale.deployed();
+
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
+
+  const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await transaction.wait()
+
+  console.log(`Tokens transferred to Crowdsale\n`)
   }
   
   // We recommend this pattern to be able to use async/await everywhere
