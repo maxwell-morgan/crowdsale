56c4623054f2ff26ac5ea51d0670434300ca887e
maxwell-morgan <maxwell@corduroycloud.com>
2023-09-30 10:43:47 -0600

    Load Accounts

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers'
+import Navigation from './Navigation';
+import Info from './Info';
+
+function App() {
+
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    console.log(provider)
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  })
+
+  return(
+    <Container>
+      <Navigation />
+      <hr />
+      {account && (
+        <Info account={account} />
+      )}
+    </Container>
+  )
+}
+
+export default App;
